fc7f70a76518c8a7c5bcde9fcdf756f77062236b
jimmymunoz <myappeu@gmail.com>
2016-12-21 14:09:25 +0100

    Initial commit

---
+++ SeniorAssistance/SeniorAssistancePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace SeniorAssistance
+{
+	public partial class SeniorAssistancePage : ContentPage
+	{
+		public SeniorAssistancePage()
+		{
+			InitializeComponent();
+		}
+	}
+}
